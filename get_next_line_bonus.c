4de048111b6075a91fe812b0006ad294112972de
Marcel Mravec <mmravec@student.42prague.com>
2024-10-02 10:13:10 +0200

    all tests passed

---
+++ get_next_line_bonus.c
@@ -0,0 +1,131 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmravec <mmravec@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/22 22:16:59 by mmravec           #+#    #+#             */
+/*   Updated: 2024/10/02 10:08:10 by mmravec          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*append_buffer(char *main_buffer, char *read_buffer);
+
+char	*extract_line(char *buffer)
+{
+	char	*temp;
+	size_t	line_length;
+	char	*line_end;
+	size_t	i;
+
+	line_end = ft_strchr(buffer, '\n');
+	if (line_end == NULL)
+		line_length = ft_strlen(buffer);
+	else
+		line_length = line_end - buffer + 1;
+	temp = (char *) ft_calloc(line_length + 1, sizeof(char));
+	if (!temp)
+		return (NULL);
+	i = 0;
+	while (i < line_length)
+	{
+		temp[i] = buffer[i];
+		i++;
+	}
+	temp[i] = '\0';
+	if (temp[0] == '\0')
+		return (free(temp), NULL);
+	return (temp);
+}
+
+char	*obtain_remaining(char *buffer)
+{
+	char	*temp;
+	char	*line_end;
+
+	line_end = ft_strchr(buffer, '\n');
+	if (line_end == NULL)
+	{
+		free(buffer);
+		return (NULL);
+	}
+	temp = (char *) ft_calloc(ft_strlen(line_end + 1) + 1, sizeof(char));
+	if (!temp)
+		return (free(buffer), NULL);
+	line_end++;
+	ft_strlcpy(temp, line_end, ft_strlen(line_end) + 1);
+	free(buffer);
+	return (temp);
+}
+
+char	*read_from_file(char *main_buffer, int fd)
+{
+	char	*local_buffer;
+	int		bytes_read;
+
+	local_buffer = ft_calloc((BUFFER_SIZE + 1), sizeof(char));
+	if (!local_buffer)
+		return (NULL);
+	bytes_read = 1;
+	while (bytes_read > 0)
+	{
+		bytes_read = read(fd, local_buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+			return (free(main_buffer), free(local_buffer), NULL);
+		local_buffer[bytes_read] = '\0';
+		main_buffer = append_buffer(main_buffer, local_buffer);
+		if (!main_buffer)
+			return (free(local_buffer), NULL);
+		if (ft_strchr(main_buffer, '\n'))
+			break ;
+	}
+	return (free(local_buffer), main_buffer);
+}
+
+char	*append_buffer(char *main_buffer, char *read_buffer)
+{
+	size_t	main_len;
+	size_t	read_len;
+	char	*temp;
+
+	if (!main_buffer)
+		main_len = 0;
+	else
+		main_len = ft_strlen(main_buffer);
+	read_len = ft_strlen(read_buffer);
+	temp = (char *)malloc(main_len + read_len + 1);
+	if (!temp)
+	{
+		free(main_buffer);
+		return (NULL);
+	}
+	if (main_buffer)
+		ft_strlcpy(temp, main_buffer, main_len + 1);
+	ft_strlcpy(temp + main_len, read_buffer, read_len + 1);
+	free(main_buffer);
+	return (temp);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*main_buffers[MAXFD];
+	char		*line;
+
+	if (fd < 0 || fd >= MAXFD || BUFFER_SIZE <= 0)
+		return (NULL);
+	main_buffers[fd] = read_from_file(main_buffers[fd], fd);
+	if (!main_buffers[fd])
+		return (NULL);
+	line = extract_line(main_buffers[fd]);
+	if (!line)
+	{
+		free(main_buffers[fd]);
+		main_buffers[fd] = NULL;
+		return (NULL);
+	}
+	main_buffers[fd] = obtain_remaining(main_buffers[fd]);
+	return (line);
+}
---
+++ get_next_line_utils.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmravec <mmravec@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/22 22:16:55 by mmravec           #+#    #+#             */
+/*   Updated: 2024/09/26 16:43:57 by mmravec          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void			*ptr;
+	size_t			i;
+	size_t			total_size;
+
+	total_size = count * size;
+	ptr = malloc(total_size);
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	while (i < total_size)
+	{
+		((unsigned char *)ptr)[i] = 0;
+		i++;
+	}
+	return (ptr);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (unsigned char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+
+	i = 0;
+	if (dstsize == 0)
+		return (ft_strlen(src));
+	while ((src[i] != '\0') && (i < dstsize - 1))
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	if (dstsize > 0)
+		dst[i] = '\0';
+	return (ft_strlen(src));
+}
